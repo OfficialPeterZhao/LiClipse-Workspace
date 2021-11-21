dbb4ac437126287e414bbe51019a25fa7e3291a7
PeterZhao <peterzhaoofficial@gmail.com>
2021-11-21 01:50:19 -0600

    print

---
+++ finallyDone/commentDrills (1).py
@@ -0,0 +1,43 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#Hello!!!!!!!!!!!
+
+#2) Make a multi-line comment.
+
+"""
+Multiline
+comment
+"""
+
+#3) Make a variable. Then put a comment after it, on the same line.
+x=5 #Helo
+
+
+#4) Make a single line comment.
+
+#kfkksadksal
+
+#5) Make a multi-line comment.
+
+"""
+sad
+man
+"""
+
