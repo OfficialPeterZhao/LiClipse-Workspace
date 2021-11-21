c0c1fc07a898fbe71b97f0b19ea5f9c4e78f4237
PeterZhao <peterzhaoofficial@gmail.com>
2021-11-21 01:39:32 -0600

    comments

---
+++ finallyDone/commentsOutline (1).py
@@ -0,0 +1,23 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+from test.test_wsgiref import hello_app
+
+#1) Make a single line comment.
+
+#Hello!
+
+#2) Make a multi-line comment.
+
+'''
+Helloo
+I
+like
+fish
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+x  = 6 #this is a variable x
