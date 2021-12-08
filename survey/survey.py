52df25cc28f136a7fecb8666f709d34fed366934
PeterZhao <peterzhaoofficial@gmail.com>
2021-12-07 18:56:07 -0600

    project

---
+++ survey/survey.py
@@ -0,0 +1,19 @@
+'''
+The purpose of this program is to take survey information
+This project is created by Peter Zhao
+'''
+
+
+name = input("What is your name?")
+
+
+
+color = input("What is your favorite color?")
+
+
+
+city = input("What city did you grow up in?")
+
+
+
+friend = input("What is your best friends name?")
