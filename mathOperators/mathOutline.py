dd883d09eec52329a26ff845ace351e8cf1e78e9
PeterZhao <peterzhaoofficial@gmail.com>
2022-01-28 22:13:56 -0600

    Math Operator Outline

---
+++ mathOperators/mathOutline.py
@@ -0,0 +1,75 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+
+x = "My " + "Name"
+print(x)
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+
+y = 4 + 4
+print(y)
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+
+x = 4 - 4
+print(x)
+
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+
+x = 9.55 - 9.44
+print(x)
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+
+x = 9 * 8
+print(x)
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+
+x = "jon " * 2
+print(x)
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+
+x = 4 / 4
+print(x)
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+
+x = 4.5/4.2
+print(X)
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+
+x = 4 % 6
+print(x)
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+
+x = 4 // 4
+print(x)
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+
+x = 5 ** 9
+print(x)
+
+#12) Make any type of operation with a common SYNTAX error
+
+x = "jon " *** 5
+print(x)
