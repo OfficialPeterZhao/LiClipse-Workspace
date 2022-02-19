7746b6826d08e1a5bf79fd2ab6d83046963f922b
PeterZhao <peterzhaoofficial@gmail.com>
2022-02-19 12:27:38 -0600

    logicalOperators

---
+++ logicalOperators/logicalOutline.py
@@ -0,0 +1,42 @@
+'''
+This outline will help solidify concepts from the Logical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+#EX) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+from pickle import TRUE, FALSE
+one = True
+two = False
+a = one and two 
+print(a)
+
+#1) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+
+three = True
+four = False
+
+a = three and four
+print(a)
+
+#2) Make two boolean variables. Put them on either side of the or operator.
+#Store this expression in a variable named b. Print the variable.
+
+nine = True
+ten = False
+
+b = nine or ten
+print(b)
+
+#3) Make one boolean variable. Put the variable after the not. Store this 
+#expression in a variable named c. Print the variable.
+
+c = not 10 == 10
+print(c)
+
+
+
+#4) Make a logical expression with one of the common SYNTAX errors.
+
+p = and 4 == 5
+print(p)
